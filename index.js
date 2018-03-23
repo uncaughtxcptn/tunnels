9c9cfdee69c1cdea3532d65dc7f16772381115f7
Arnelle Balane <arnellebalane@gmail.com>
2018-03-24 00:54:50 +0800

    feat: initial hapi server implementation

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const { to } = require('await-to-js');
+const hapi = require('hapi');
+
+const server = hapi.server({
+    host: 'localhost',
+    port: process.env.PORT
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler(request, h) {
+        return 'hello hapi!';
+    }
+});
+
+async function start() {
+    const [ err ] = await to(server.start());
+
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+
+    console.log('Server is running:', server.info.uri);
+}
+
+start();
